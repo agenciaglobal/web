5e32022c5d606138b2287d6caf91a8cc92d6c057
cescoferraro <francescoaferraro@gmail.com>
2020-07-23 17:30:53 -0300

    remove mdx plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,36 +77,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-mdx`,
-      options: {
-        extensions: [".mdx", ".md"],
-        gatsbyRemarkPlugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590,
-            },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-          {
-            resolve: `gatsby-remark-vscode`,
-          },
-          {
-            resolve: `gatsby-remark-copy-linked-files`,
-          },
-          {
-            resolve: `gatsby-remark-smartypants`,
-          },
-        ],
-        plugins: [`gatsby-remark-images`],
-      },
-    },
-    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // edit below
