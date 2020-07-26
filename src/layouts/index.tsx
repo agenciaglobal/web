243b548a9f6ac3ead295ea1aedfeff22df9da934
cescoferraro <francescoaferraro@gmail.com>
2020-07-26 18:53:01 -0300

    local storage light mode state storage

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -3,17 +3,23 @@ import * as React from "react"
 import { darkTheme, LightMode, lightTheme } from "../shared/theme"
 import "./global.css"
 import { ActualLayout } from "./main"
+import { useLocalStorage } from "./useLocalStorage"
 
-const Layout = (props: { uri: string }): React.ReactElement => {
-  const [mode, setMode] = React.useState<LightMode>("dark")
+interface Props {
+  uri: string
+  children: React.ReactElement
+}
+
+const Layout = ({ children, uri }: Props): React.ReactElement => {
+  const [mode, setMode] = useLocalStorage<LightMode>("gblm", "dark")
   return (
     <ThemeProvider theme={mode === "light" ? lightTheme : darkTheme}>
       <ActualLayout
-        uri={props.uri}
+        uri={uri}
         lightMode={mode}
         toggleLightMode={() => setMode(mode === "light" ? "dark" : "light")}
       >
-        {props.children}
+        {children}
       </ActualLayout>
     </ThemeProvider>
   )
