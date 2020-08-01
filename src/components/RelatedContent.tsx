ad005f57d943040421c86062d55e4c35bb459f93
cescoferraro <francescoaferraro@gmail.com>
2020-08-01 10:29:49 -0300

    minor stuff

---
+++ src/components/RelatedContent.tsx
@@ -0,0 +1,68 @@
+import { Box } from "@material-ui/core"
+import { makeStyles, Theme } from "@material-ui/core/styles"
+import Typography from "@material-ui/core/Typography"
+import { Link } from "gatsby-plugin-react-i18next"
+import React from "react"
+import ScrollMenu from "react-horizontal-scrolling-menu"
+import { Maybe, SitePageContextNews } from "../global"
+import { ExpandTExt } from "./NewsPage/components/expand_text"
+
+export const useStyles = makeStyles((theme: Theme) => ({
+  type: {
+    display: "flex",
+    marginTop: 40,
+    marginBottom: 40,
+    flexDirection: "column",
+    backgroundColor: theme.palette.secondary.main,
+    height: 660,
+  },
+}))
+export const RelatedContent = (props: {
+  news: Array<Maybe<SitePageContextNews>>
+}) => {
+  const classes = useStyles()
+  return (
+    <Box className={classes.type}>
+      <Box
+        style={{
+          height: 150,
+          display: "flex",
+          justifyContent: "flex-start",
+          alignItems: "center",
+          marginLeft: 24,
+        }}
+      >
+        <Typography variant={"h2"} noWrap={false} style={{ fontSize: 30 }}>
+          Conteúdo Relacionado
+        </Typography>
+      </Box>
+      <ScrollMenu
+        alignCenter={false}
+        data={props.news
+          .map((d) => d.node)
+          .map((el, index) => {
+            /* eslint-disable  @typescript-eslint/no-var-requires */
+            const image = require("../../content/" + el.frontmatter.image)
+            return (
+              <Link
+                style={{ textDecoration: "none" }}
+                key={index}
+                to={"/news" + el.fields.slug}
+              >
+                <div
+                  className={`menu-item`}
+                  style={{ background: `url(${image})` }}
+                >
+                  <ExpandTExt
+                    date={el.frontmatter.date}
+                    title={el.frontmatter.title}
+                    description={el.frontmatter.description}
+                  />
+                </div>
+              </Link>
+            )
+          })}
+      />
+    </Box>
+  )
+}
--- src/templates/news-post.tsx
+++ src/templates/news-post.tsx
@@ -1,83 +1,14 @@
-import { Box } from "@material-ui/core"
-import { makeStyles, Theme } from "@material-ui/core/styles"
 import Typography from "@material-ui/core/Typography"
 import { graphql } from "gatsby"
-import { Link } from "gatsby-plugin-react-i18next"
 import React from "react"
-import ScrollMenu from "react-horizontal-scrolling-menu"
-import { ExpandTExt } from "../components/NewsPage/components/expand_text"
 import { NewsPage } from "../components/NewsPage/newsPage"
-import {
-  Maybe,
-  NewsPostBySlugQuery,
-  SitePageContext,
-  SitePageContextNews,
-} from "../global"
+import { RelatedContent } from "../components/RelatedContent"
+import { NewsPostBySlugQuery, SitePageContext } from "../global"
 import "./scroll.css"
-
-export const useStyles = makeStyles((theme: Theme) => ({
-  type: {
-    display: "flex",
-    marginTop: 40,
-    marginBottom: 40,
-    flexDirection: "column",
-    backgroundColor: theme.palette.secondary.main,
-    height: 660,
-  },
-}))
 
 interface Props {
   data?: NewsPostBySlugQuery
   pageContext: SitePageContext
-}
-
-const RelatedContent = (props: { news: Array<Maybe<SitePageContextNews>> }) => {
-  const classes = useStyles()
-  return (
-    <Box className={classes.type}>
-      <Box
-        style={{
-          height: 150,
-          display: "flex",
-          justifyContent: "flex-start",
-          alignItems: "center",
-          marginLeft: 24,
-        }}
-      >
-        <Typography variant={"h2"} noWrap={false} style={{ fontSize: 30 }}>
-          Conteúdo Relacionado
-        </Typography>
-      </Box>
-      <ScrollMenu
-        alignCenter={false}
-        data={props.news
-          .map((d) => d.node)
-          .map((el, index) => {
-            /* eslint-disable  @typescript-eslint/no-var-requires */
-            const image = require("../../content/" + el.frontmatter.image)
-            return (
-              <Link
-                style={{ textDecoration: "none" }}
-                key={index}
-                to={"/news" + el.fields.slug}
-              >
-                <div
-                  className={`menu-item`}
-                  style={{ background: `url(${image})` }}
-                >
-                  <ExpandTExt
-                    date={el.frontmatter.date}
-                    title={el.frontmatter.title}
-                    description={el.frontmatter.description}
-                  />
-                </div>
-              </Link>
-            )
-          })}
-        selected={"item1"}
-      />
-    </Box>
-  )
 }
 
 const NewsPostTemplate = (props: Props): React.ReactElement => {
