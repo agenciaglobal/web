cc4090085d69d3fc8f780a2e7efc8d4c773c4b56
matheus <matheuskerpen@gmail.com>
2020-08-10 13:56:49 -0300

    pretyy

--- src/components/HomePage/title.tsx
+++ src/components/HomePage/title.tsx
@@ -8,22 +8,22 @@ const useGridStyles = makeStyles(() => ({
     marginTop: 24,
     fontFamily: "GSThree",
     fontSize: "30px",
-    '@media (min-width:600px)': {
-      fontSize: '40px',
-      },
+    "@media (min-width:600px)": {
+      fontSize: "40px",
+    },
     lineHeight: "1.4em",
     textTransform: "capitalize",
-    paddingBottom: 8
+    paddingBottom: 10,
   },
   subtitle: {
     fontWeight: 500,
     fontSize: "18px",
-    '@media (min-width:600px)': {
-      fontSize: '27px',
-      },
+    "@media (min-width:600px)": {
+      fontSize: "27px",
+    },
     lineHeight: "1.4em",
     textTransform: "capitalize",
-    paddingBottom: 45
+    paddingBottom: 45,
   },
 }))
 export const HomeTitleComponent = (): React.ReactElement => {
