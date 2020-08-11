c25c2a0688c698692267f57b6136b102be890c5f
Matheus Kerpen <36003926+matheuskerpen@users.noreply.github.com>
2020-08-11 17:08:14 -0300

    conteúdo das sanfoninhas (#30)

--- src/components/AboutPage/accordion_date.tsx
+++ src/components/AboutPage/accordion_date.tsx
@@ -1,26 +1,78 @@
 export const accordionData = [
   {
     title: "ESTRATÉGIA",
-    text: "sdkjf",
+    text: [
+      "Estratégia de marca e posicionamento",
+      "Dna de marca",
+      "Business design",
+      "Estratégia de experiência do consumidor",
+      "Mapeamento da jornada do consumidor",
+      "Estratégia de conteúdo",
+      "Pesquisa com usuário",
+      "Pesquisa de mercado"
+    ]
   },
   {
     title: "DATA INTELLIGENCE & ANALYTICS",
-    text: "sdkjf",
+    text: [
+      "Estratégia e mapeamento Dados",
+      "LGPD compliance",
+      "Dashboards Real-time",
+      "Social Listening",
+      "Brand Health",
+      "Insights de Consumidor - 1st and 3rd party data",
+      "Segmentação de Audiência - 1st party data",
+      "Desenvolvimento de KPI",
+      "Report de marketing digital"
+    ]
   },
   {
     title: "COMUNICAÇÃO",
-    text: "sdkjf",
+    text: [
+      "Planejamento de comunicação",
+      "Desenvolvimento de campanha integrada - on e off",
+      "Desenvolvimento de campanha integrada - narrativa e interativa",
+      "Desenvolvimento e produção de conteúdo digital display",
+      "Print & outdoor",
+      "Vídeo & tv",
+      "Crm - canais diretos da marca"
+    ]
   },
   {
     title: "DESIGN",
-    text: "sdkjf",
+    text: [
+      "Identidade de marca",
+      "Naming",
+      "Embalagem",
+      "Estratégia e design de ux/ui",
+      "Design Interface de experiência",
+      "Design de hotsite/landing page",
+      "Prototipação",
+      "Design web responsivo",
+      "Native apps"
+    ]
   },
   {
     title: "MÍDIAS E PLATAFORMAS",
-    text: "sdkjf",
+    text: [
+      "Planejamento de mídia",
+      "Compra de mídia",
+      "Planejamento de canais",
+      "Connections Planning",
+      "Mensuração e otimização"
+    ]
   },
   {
     title: "PERFORMANCE",
-    text: "sdkjf",
+    text: [
+      "Search Engine Marketing (SEM)",
+      "Search Engine Optimization (SEO)",
+      "Compra Programática de Display e Vídeo (RTB - Real Time Buying)",
+      "Social media pago (Facebook, Instagram, Messenger, Twitter, and Linkedin)",
+      "Otimização de resposta direta (Lead, Website Purchase, App Installs, App Purchase)",
+      "Taggemento e otimização de landing page",
+      "E-mail marketing e otimização",
+      "Campanhas de remarketing em Mobile, APP, and Desktop campaigns"
+    ]
   },
 ]
