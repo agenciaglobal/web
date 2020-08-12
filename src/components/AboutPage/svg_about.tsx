57a3498fae9d184683912721ad1108138c3f9a85
matheus <matheuskerpen@gmail.com>
2020-08-12 14:04:37 -0300

    no errors

--- src/components/AboutPage/svg_about.tsx
+++ src/components/AboutPage/svg_about.tsx
@@ -2,7 +2,7 @@ import { Typography } from "@material-ui/core"
 import Box from "@material-ui/core/Box"
 import * as React from "react"
 import { useAboutSvgStyles } from "./about_styles"
-import { SVGGlobalCool } from "./svg"
+// import { SVGGlobalCool } from "./svg"
 import video from "../../../static/videos/mov.mp4"
 
 export const SVGAbout = (): React.ReactElement => {
@@ -18,8 +18,13 @@ export const SVGAbout = (): React.ReactElement => {
         </Typography>
       </Box>
       <Box className={classes.box}>
-          {/* <SVGGlobalCool/> */}
-          <video src={video} autoPlay loop style={{ maxWidth: 900, width: "100%", height: "auto"}}/>
+        {/* <SVGGlobalCool/> */}
+        <video
+          src={video}
+          autoPlay
+          loop
+          style={{ maxWidth: 900, width: "100%", height: "auto" }}
+        />
       </Box>
     </Box>
   )
