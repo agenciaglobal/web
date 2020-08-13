1cfe128675a8765ea2aeb3a761cb519e86f2e4bf
matheus <matheuskerpen@gmail.com>
2020-08-13 14:54:57 -0300

    oops

--- src/components/AboutPage/grid_text.tsx
+++ src/components/AboutPage/grid_text.tsx
@@ -52,8 +52,8 @@ export const AboutTextGrid = (): React.ReactElement => {
         <Typography className={classes.div}>
           Acreditamos que só quem consegue navegar à mudança são aqueles que
           criativamente procuram novos caminhos.
-        </Box>
       </Typography>
+      </Box>
       <Typography className={classes.div2}>
         <Box lineHeight={1.5}>Change requires Creativity.</Box>
       </Typography>
