b0d591e9ec50f6fab70a954bc4632864ac827384
cescoferraro <francescoaferraro@gmail.com>
2020-08-16 11:01:07 -0300

    about fix

--- src/components/AboutPage/aboutPage.tsx
+++ src/components/AboutPage/aboutPage.tsx
@@ -1,21 +1,21 @@
-import Grid from "@material-ui/core/Grid"
-import makeStyles from "@material-ui/core/styles/makeStyles"
 import * as React from "react"
-import { AboutAccordion } from "./accordion"
+import Grid from "@material-ui/core/Grid"
 import { AboutImageGrid } from "./grid_image"
+import { AboutAccordion } from "./accordion"
 import { AboutTextGrid } from "./grid_text"
+import makeStyles from "@material-ui/core/styles/makeStyles"
 import { SVGAbout } from "./svg_about"
 
 const useGridStyles = makeStyles(() => ({
   div: { flexGrow: 1, paddingTop: 30 },
-  grid: { padding: 0 },
+  grid: { padding: 0, alignItems: "stretch" },
 }))
 
 export const AboutPage = (): React.ReactElement => {
   const classes = useGridStyles()
   return (
     <div className={classes.div}>
-      <Grid className={classes.grid} alignItems="center" container spacing={8}>
+      <Grid className={classes.grid} alignItems="center" container>
         <AboutTextGrid />
         <AboutImageGrid />
       </Grid>
--- src/components/AboutPage/grid_text.tsx
+++ src/components/AboutPage/grid_text.tsx
@@ -1,10 +1,10 @@
-import { Typography } from "@material-ui/core"
+import { Typography, Theme } from "@material-ui/core"
 import Box from "@material-ui/core/Box"
 import Grid from "@material-ui/core/Grid"
 import makeStyles from "@material-ui/core/styles/makeStyles"
 import * as React from "react"
 
-const useGridStyles = makeStyles(() => ({
+const useGridStyles = makeStyles((theme: Theme) => ({
   div: {
     textAlign: "justify",
     fontFamily: "GSTwo",
@@ -28,44 +28,54 @@ const useGridStyles = makeStyles(() => ({
   grid: {
     padding: 0,
   },
+  box: {
+    padding: 24,
+    paddingTop: 0,
+    [theme.breakpoints.down("xs")]: {
+      padding: 0,
+      paddingTop: 0,
+    },
+  },
 }))
 
 export const AboutTextGrid = (): React.ReactElement => {
   const classes = useGridStyles()
   return (
     <Grid className={classes.grid} item xs={12} sm={7}>
-      <Box lineHeight={3}>
-        <Typography className={classes.div}>
-          Em um contexto onde a única certeza é a mudança e sua velocidade,
-          somos o parceiro para ajudar os negócios a navegá-la com clareza
-          estratégica e criatividade.
+      <Box className={classes.box}>
+        <Box lineHeight={3}>
+          <Typography className={classes.div}>
+            Em um contexto one a única certeza é a mudança e sua velocidade,
+            somos o parceiro para ajudar os negócios a navegá-la com clareza
+            estratégica e criatividade.
+          </Typography>
+        </Box>
+        <Box lineHeight={3}>
+          <Typography className={classes.div}>
+            O parceiro para conectar as pontas. Para dizer o que tem que ser
+            dito, mas de um jeito diferente. Para fazer conteúdo que construa
+            uma audiência engajada. Para reunir comunicação, tecnologia, dados e
+            design em prol da construção de marcas contemporâneas.
+          </Typography>
+        </Box>
+        <Box lineHeight={3}>
+          <Typography className={classes.div}>
+            Chamamos de crowdtalent a junção dos melhores talentos com
+            diferentes skills e conhecimentos em torno de uma ideia. Um processo
+            integrado de trabalho que nos permite avançar juntos dos nossos
+            clientes na transformação de suas marcas.
+          </Typography>
+        </Box>
+        <Box lineHeight={3}>
+          <Typography className={classes.div}>
+            Acreditamos que só quem consegue navegar à mudança são aqueles que
+            criativamente procuram novos caminhos.
+          </Typography>
+        </Box>
+        <Typography className={classes.div2}>
+          <Box lineHeight={1.5}>Change requires Creativity.</Box>
         </Typography>
       </Box>
-      <Box lineHeight={3}>
-        <Typography className={classes.div}>
-          O parceiro para conectar as pontas. Para dizer o que tem que ser dito,
-          mas de um jeito diferente. Para fazer conteúdo que construa uma
-          audiência engajada. Para reunir comunicação, tecnologia, dados e
-          design em prol da construção de marcas contemporâneas.
-        </Typography>
-      </Box>
-      <Box lineHeight={3}>
-        <Typography className={classes.div}>
-          Chamamos de crowdtalent a junção dos melhores talentos com diferentes
-          skills e conhecimentos em torno de uma ideia. Um processo integrado de
-          trabalho que nos permite avançar juntos dos nossos clientes na
-          transformação de suas marcas.
-        </Typography>
-      </Box>
-      <Box lineHeight={3}>
-        <Typography className={classes.div}>
-          Acreditamos que só quem consegue navegar à mudança são aqueles que
-          criativamente procuram novos caminhos.
-        </Typography>
-      </Box>
-      <Typography className={classes.div2}>
-        <Box lineHeight={1.5}>Change requires Creativity.</Box>
-      </Typography>
     </Grid>
   )
 }
