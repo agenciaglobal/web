1445752314c25a24c69de074d80e44b9de431977
matheus <matheuskerpen@gmail.com>
2020-08-24 21:17:16 -0300

    finaly fixed

--- src/components/HomePage/types.d.ts
+++ src/components/HomePage/types.d.ts
@@ -3,6 +3,12 @@ import { Maybe, MdxFrontmatter } from "global"
 export type Portifolio = { slug: string } & Maybe<
   Pick<
     MdxFrontmatter,
-    "video" | "cover_image" | "date" | "title" | "description" | "categorie"
+    | "video"
+    | "cover_image"
+    | "date"
+    | "title"
+    | "description"
+    | "categorie"
+    | "image"
   >
 >
