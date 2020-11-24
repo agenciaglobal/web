9adecff2cdcc4be9ac8560e8e8535844188bcb97
matheus <matheuskerpen@gmail.com>
2020-11-24 16:13:50 -0300

    UA-52982227-1

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "GTM-N26KCFV",
+        trackingId: "UA-52982227-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
       }
